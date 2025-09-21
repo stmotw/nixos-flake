e201179412a9a3111065c9c4f48e98b2e8e165ac
Timofey Urbanovich <timofey.urbanovich@ai71.ai>
2025-09-21 21:57:33 +0400

    feat(work-mac): enable docker desktop

---
+++ modules/darwin/services/docker/default.nix
@@ -0,0 +1,15 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.mine.services.docker;
+in {
+  options.mine.services.docker = {
+    enable = lib.mkEnableOption "Docker desktop";
+  };
+
+  config = lib.mkIf cfg.enable {
+    homebrew.casks = ["docker"];
+  };
+}
