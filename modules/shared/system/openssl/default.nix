a8d4915d4bb6df1aa4d956d0a7f6ac038284da3d
Timofey Urbanovich <motw@yandex.ru>
2026-01-02 11:32:16 +0400

    feat(arvad): add openssl

---
+++ modules/shared/system/openssl/default.nix
@@ -0,0 +1,18 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.mine.system.openssl;
+in {
+  options.mine.system.openssl = {
+    enable = lib.mkEnableOption "Enable openssl";
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      openssl
+    ];
+  };
+}
