ad0a502c2cf5cc42cf5bbb9774a9d41e6977860f
Timofey Urbanovich <motw@yandex.ru>
2026-01-02 11:33:40 +0400

    feat(git): use author's email instead of author's name in git br and l commands

--- modules/home/cli-tools/git/default.nix
+++ modules/home/cli-tools/git/default.nix
@@ -30,8 +30,8 @@ in {
           s = "status";
           co = "checkout";
           cob = "checkout -b";
-          br = "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate";
-          l = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+          br = "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authoremail)]' --sort=-committerdate";
+          l = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %ae%C(reset)%C(bold yellow)%d%C(reset)' --all";
           c = "commit -v";
         };
 
