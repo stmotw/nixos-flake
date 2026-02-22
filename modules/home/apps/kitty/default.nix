51935ae7b56f3466fbec7d6a7db6c38a1d5e4c01
Timofey Urbanovich <motw@yandex.ru>
2026-02-22 18:08:51 +0400

    feat(kitty): follow system switch for light / dark theme

--- modules/home/apps/kitty/default.nix
+++ modules/home/apps/kitty/default.nix
@@ -6,6 +6,60 @@
 }: let
   inherit (config.mine) user;
   cfg = config.mine.home-manager.apps.kitty;
+
+  # Matches stylix kitty color config
+  # https://github.com/nix-community/stylix/blob/master/modules/kitty/hm.nix
+  mkKittyColors = scheme: ''
+    background #${scheme.base00}
+    foreground #${scheme.base05}
+    selection_background #${scheme.base03}
+    selection_foreground #${scheme.base05}
+
+    cursor #${scheme.base05}
+    cursor_text_color #${scheme.base00}
+
+    url_color #${scheme.base04}
+
+    active_border_color #${scheme.base03}
+    inactive_border_color #${scheme.base01}
+
+    wayland_titlebar_color #${scheme.base00}
+    macos_titlebar_color #${scheme.base00}
+
+    active_tab_background #${scheme.base00}
+    active_tab_foreground #${scheme.base05}
+    inactive_tab_background #${scheme.base01}
+    inactive_tab_foreground #${scheme.base04}
+    tab_bar_background #${scheme.base01}
+
+    color0 #${scheme.base00}
+    color1 #${scheme.base08}
+    color2 #${scheme.base0B}
+    color3 #${scheme.base0A}
+    color4 #${scheme.base0D}
+    color5 #${scheme.base0E}
+    color6 #${scheme.base0C}
+    color7 #${scheme.base05}
+
+    color8 #${scheme.base02}
+    color9 #${scheme.base08}
+    color10 #${scheme.base0B}
+    color11 #${scheme.base0A}
+    color12 #${scheme.base0D}
+    color13 #${scheme.base0E}
+    color14 #${scheme.base0C}
+    color15 #${scheme.base07}
+
+    color16 #${scheme.base09}
+    color17 #${scheme.base0F}
+    color18 #${scheme.base01}
+    color19 #${scheme.base02}
+    color20 #${scheme.base04}
+    color21 #${scheme.base06}
+  '';
+
+  darkColors = config.mine.system.stylix.darkBase16Scheme;
+  lightColors = config.mine.system.stylix.lightBase16Scheme;
 in {
   options.mine.home-manager.apps.kitty = {
     enable = lib.mkEnableOption "Enable kitty terminal emulator";
@@ -17,10 +71,19 @@ in {
         kitty
       ];
 
+      stylix.targets.kitty.enable = false;
+
       programs.kitty = {
         enable = true;
         shellIntegration.enableZshIntegration = config.mine.home-manager.system.shell.zsh.enable;
+        font = {
+          inherit (config.stylix.fonts.monospace) package name;
+          size = config.stylix.fonts.sizes.terminal;
+        };
       };
+
+      xdg.configFile."kitty/dark-theme.auto.conf".text = mkKittyColors darkColors;
+      xdg.configFile."kitty/light-theme.auto.conf".text = mkKittyColors lightColors;
     };
   };
 }
--- modules/shared/system/stylix/default.nix
+++ modules/shared/system/stylix/default.nix
@@ -5,34 +5,64 @@
   ...
 }: let
   cfg = config.mine.system.stylix;
+
+  # Adaptation of VSCode's Dark Modern theme
+  darkScheme = {
+    base00 = "1E1E1E";
+    base01 = "181818";
+    base02 = "252526";
+    base03 = "3A3D41";
+    base04 = "6B6B6B";
+    base05 = "D4D4D4";
+    base06 = "CCCCCC";
+    base07 = "FFFFFF";
+    base08 = "D16969";
+    base09 = "CE9178";
+    base0A = "DCDCAA";
+    base0B = "608B4E";
+    base0C = "4EC9B0";
+    base0D = "569CD6";
+    base0E = "C586C0";
+    base0F = "D7BA7D";
+  };
+
+  lightScheme = {
+    base00 = "F8F8F8";
+    base01 = "E8E8E8";
+    base02 = "D8D8D8";
+    base03 = "B8B8B8";
+    base04 = "585858";
+    base05 = "383838";
+    base06 = "282828";
+    base07 = "181818";
+    base08 = "AB4642";
+    base09 = "DC9656";
+    base0A = "F7CA88";
+    base0B = "A1B56C";
+    base0C = "86C1B9";
+    base0D = "7CAFC2";
+    base0E = "BA8BAF";
+    base0F = "A16946";
+  };
 in {
   options.mine.system.stylix = {
     enable = lib.mkEnableOption "stylix color scheme";
+    darkBase16Scheme = lib.mkOption {
+      type = lib.types.attrs;
+      default = darkScheme;
+      description = "Dark base16 color scheme for apps supporting light/dark switching";
+    };
+    lightBase16Scheme = lib.mkOption {
+      type = lib.types.attrs;
+      default = lightScheme;
+      description = "Light base16 color scheme for apps supporting light/dark switching";
+    };
   };
 
   config = lib.mkIf cfg.enable {
     stylix = {
       enable = true;
-
-      # Adaptation of VSCode's Dark Modern theme
-      base16Scheme = {
-        base00 = "1E1E1E";
-        base01 = "181818";
-        base02 = "252526";
-        base03 = "3A3D41";
-        base04 = "6B6B6B";
-        base05 = "D4D4D4";
-        base06 = "CCCCCC";
-        base07 = "FFFFFF";
-        base08 = "D16969";
-        base09 = "CE9178";
-        base0A = "DCDCAA";
-        base0B = "608B4E";
-        base0C = "4EC9B0";
-        base0D = "569CD6";
-        base0E = "C586C0";
-        base0F = "D7BA7D";
-      };
+      base16Scheme = darkScheme;
 
       fonts = {
         monospace = {
